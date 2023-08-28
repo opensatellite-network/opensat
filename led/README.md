2119b61d8d10eae4f6d7bdbb17af01d13f417622
krishpranav <krisna.pranav@gmail.com>
2023-08-28 19:53:48 +0530

    opensat: led functionalities

---
+++ led/README.md
@@ -0,0 +1,1 @@
+## LED for satellite.
---
+++ led/src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file main.cpp
+ * @author Krisna Pranav aka (krishpranav)
+ * @brief open satellite LED functioalities.
+ * @version 1.0
+ * @date 2023-08-28
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, OpenSatelliteNetwork Developers
+ * 
+ */
+
